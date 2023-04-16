f72f2a4eb9e13b886a8eb7002a7ffe009e5c6e17
abhinay5993 <abhinaylunawat5993@gmail.com>
2023-04-16 17:08:51 +0530

    adding concepts of Migration + django Admin

--- pyDjangoSampleApp/geekstest_app/appmod1_login/models.py
+++ pyDjangoSampleApp/geekstest_app/appmod1_login/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from model_utils import Choices
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
 class PersonModel(models.Model):
     # id = models.BigAutoField(primary_key=True)
-    GENDER = [
-        ("M", "Male"),
-        ("F", "Female"),
-        ("O", "Others"),
-    ]
+    GENDER = Choices(
+        (1, "male", _("male")),
+        (2, "female", _("female")),
+        (3, "others", _("others")),
+    )
     fname = models.CharField(max_length=50)
     lstName = models.CharField(max_length=50)
-    ageVal = models.IntegerField(default=0, null=True)
     emailVal = models.EmailField()
-    genderVal = models.CharField(max_length=1, choices=GENDER)
-      
+    genderVal = models.IntegerField(choices=GENDER, default=GENDER.male)
+    ageVal = models.IntegerField(default=0, null=True)
+
 # class Meta:
 #       db_table = "testTableCustTableName"
